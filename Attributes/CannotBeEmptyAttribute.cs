6a9bd35d74af09c3549d8364c9acdc1f9d22d873
Kerry Walker <kerryw@outlook.com>
2019-03-15 14:41:22 +0000

    Update CannotBeEmptyAttribute.cs

--- Attributes/CannotBeEmptyAttribute.cs
+++ Attributes/CannotBeEmptyAttribute.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace WebAPI
+namespace WebAPI.Attributes
 {
 	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 	public sealed class CannotBeEmptyAttribute : RequiredAttribute
