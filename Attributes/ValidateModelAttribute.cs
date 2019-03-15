722cffbf789fa9072715c5c1447ee7f4c915bfc2
Kerry Walker <kerryw@outlook.com>
2019-03-15 14:35:32 +0000

    Create ValidateModelAttribute.cs

---
+++ Attributes/ValidateModelAttribute.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.Validation
+{
+	public class ValidateModelAttribute : ActionFilterAttribute
+	{
+		
+		public override void OnActionExecuting(HttpActionContext actionContext)
+		{
+			if (!actionContext.ModelState.IsValid)
+			{
+				actionContext.Response =  actionContext.Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, actionContext.ModelState);
+			}	
+		}
+	}
+}
