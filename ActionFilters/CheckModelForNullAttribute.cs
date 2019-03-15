112e42b681a125f5f9fc767dc670334a4834defe
Kerry Walker <kerryw@outlook.com>
2019-03-15 14:44:58 +0000

    Create CheckModelForNullAttribute.cs

---
+++ ActionFilters/CheckModelForNullAttribute.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class CheckModelForNullAttribute : ActionFilterAttribute
+    {
+        private readonly Func<Dictionary<string, object>, bool> _validate;
+
+        public CheckModelForNullAttribute() : this(arguments => arguments.ContainsValue(null))
+        {
+        }
+
+        public CheckModelForNullAttribute(Func<Dictionary<string, object>, bool> checkCondition)
+        {
+            _validate = checkCondition;
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (_validate(actionContext.ActionArguments))
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+       HttpStatusCode.BadRequest, string.Format("The {0} cannot be null", actionContext.ActionArguments.First().Key));
+            }
+        }
+    }
+}
