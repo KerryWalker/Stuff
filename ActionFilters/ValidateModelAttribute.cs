c8658015db56d90900ff20fff3c9106765e37378
Kerry Walker <kerryw@outlook.com>
2019-03-15 14:40:54 +0000

    Update and rename Attributes/ValidateModelAttribute.cs to ActionFilters/ValidateModelAttribute.cs

--- ActionFilters/ValidateModelAttribute.cs
+++ ActionFilters/ValidateModelAttribute.cs
@@ -8,11 +8,10 @@ using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
-namespace WebAPI.Validation
+namespace WebAPI.ActionFilters
 {
 	public class ValidateModelAttribute : ActionFilterAttribute
 	{
-		
 		public override void OnActionExecuting(HttpActionContext actionContext)
 		{
 			if (!actionContext.ModelState.IsValid)
